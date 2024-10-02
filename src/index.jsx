234704db21f144bc7955a59c70c5496766d66fc2
aeonshad <shadaeon@gmail.com>
2024-10-02 09:26:04 +0200

    🌗 Feature: Added ThemeToggle component for light/dark mode switching

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'next-themes';
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
 
