90f9700b3144e423f09b6f904b219d0467c5d4af
aeonshad <shadaeon@gmail.com>
2024-10-02 09:56:05 +0200

    🛣️ Feature: Implemented routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Footer from './layouts/Footer';
 import Header from './layouts/Header';
+import Home from './pages/home';
+import Employees from './pages/employees';
+import CreateEmployee from './pages/employees/createEmployee';
 
 function App() {
     return (
-        <div>
+        <Router>
             <Header />
-            <div className="flex-1"></div>
+            <div className="flex-1">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/employees" element={<Employees />} />
+                    <Route path="/employees/create" element={<CreateEmployee />} />
+                </Routes>
+            </div>
             <Footer />
-        </div>
+        </Router>
     );
 }
 
