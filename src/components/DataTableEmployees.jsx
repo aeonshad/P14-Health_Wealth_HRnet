61016ffe4ad42a10548e008e7913a1ed17e6314c
aeonshad <shadaeon@gmail.com>
2024-10-02 12:56:15 +0200

    🔍 Feature: Implemented search functionality in DataTable

--- src/components/DataTableEmployees.jsx
+++ src/components/DataTableEmployees.jsx
@@ -1,7 +1,41 @@
 import DataTable from 'react-data-table-component';
 import { columns } from '../utils/userDataTable';
+import { useEffect, useState } from 'react';
 
 function DataTableEmployees({ employees }) {
-    return <DataTable columns={columns} data={employees} pagination paginationRowsPerPageOptions={[10, 25, 50, 100]} fixedHeader highlightOnHover />;
+    const [data, setData] = useState(employees);
+    const [search, setSearch] = useState('');
+    useEffect(() => {
+        if (search.length > 0) {
+            const result = employees.filter((item) => {
+                return item.firstname.toLowerCase().match(search.toLocaleLowerCase()) || item.lastname.toLowerCase().match(search.toLocaleLowerCase());
+            });
+            setData(result);
+        } else {
+            setData(employees);
+        }
+    }, [search, employees]);
+
+    return (
+        <DataTable
+            columns={columns}
+            data={data}
+            noDataComponent={`No match for ${search}`}
+            pagination
+            paginationRowsPerPageOptions={[10, 25, 50, 100]}
+            fixedHeader
+            highlightOnHover
+            subHeader
+            subHeaderComponent={
+                <input
+                    className="flex h-9 rounded-md w-full border bg-card border-border px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder-muted focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                    type="text"
+                    placeholder="Search..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            }
+        />
+    );
 }
 export default DataTableEmployees;
