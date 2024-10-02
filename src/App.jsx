b5d7c520b826c23bd820c326be79f75624fe79fb
aeonshad <shadaeon@gmail.com>
2024-10-02 09:18:30 +0200

    ✨ Layout: Added Header and Footer layouts to structure the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Footer from './layouts/Footer';
+import Header from './layouts/Header';
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Header />
+            <div className="flex-1"></div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
